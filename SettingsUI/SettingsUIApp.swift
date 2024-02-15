ba7abcf227fc10f9c5867431b0f21238edfad0a7
Vladyslav Dikhtiaruk <vlad.dikhtiaruk@gmail.com>
2024-02-15 19:48:58 +0100

    Initial Commit

---
+++ SettingsUI/SettingsUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SettingsUIApp.swift
+//  SettingsUI
+//
+//  Created by Vladyslav Dikhtiaruk on 15/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct SettingsUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
